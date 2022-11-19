7ed766100c8ea31ebaec23d7ce5e2fe97cf65dfb
Alexandr Zhdanko <zhdko@yandex.ru>
2022-11-19 05:40:10 +0400

    feat: add task popup

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,19 @@ $(document).ready(function() {
         $('.page').toggleClass('page__shadow');
 	});
 });
+
+const popup = document.querySelector('.popup');
+const addTask = document.querySelector('.btn_action_add');
+const closeButton = document.querySelector('.btn_action_close');
+const taskName = document.querySelector('.add-tasks__input');
+const taskTitle = document.querySelector('.popup__text_type_task');
+
+taskTitle.value = taskName.value
+
+function togglePopup() {
+  popup.classList.toggle('popup_opened');
+}
+
+addTask.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+
